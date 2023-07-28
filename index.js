067648cd44ec1b99ecff2d36228c53d9c5298602
Picasso <urvishp13@gmail.com>
2023-07-28 13:29:10 -0400

    able to set password length between 8 and 15 =)

--- index.js
+++ index.js
@@ -37,10 +37,17 @@ pswdLength.addEventListener("mousedown", function() {
     clicked = true
 })
 
+let lastY
 // while the mouse is being dragged after clicking on the password length field 
 document.addEventListener("mousemove", function(e) {
     if (clicked === true) {
-        pswdLength.textContent++
+        if (e.y < lastY) { // mouse moved up
+            pswdLength.textContent < 15 ? pswdLength.textContent++ : pswdLength.textContent
+        } else { // mouse moved down
+            pswdLength.textContent > 8 ? pswdLength.textContent-- : pswdLength.textContent
+        }
+
+        lastY = e.y
     }
 })
 
