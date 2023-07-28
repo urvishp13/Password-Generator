c48b3f96d1260881c0517a720a1dbca2708fcd5f
Picasso <urvishp13@gmail.com>
2023-07-28 14:54:35 -0400

    completed functionality

--- assets/javascript/generatePassword.js
+++ assets/javascript/generatePassword.js
@@ -1,11 +1,12 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-     "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", 
-     "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", 
-     "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+const alphabet = "ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const specialChars = "`~!@#$%^&*()-_=+[]{}|;:'\",<.>/\\?"
+const numbers = "1234567890"
+
 const generatePasswordBtn = document.getElementById("generate-passwords-btn")
 const password1 = document.getElementById("password-1")
 const password2 = document.getElementById("password-2")
-
+const includeNums = document.getElementById("include-numbers")
+const includeSpecialChars = document.getElementById("include-special-chars")
 
 // when User clicks "generate passwords"
 generatePasswordBtn.addEventListener("click", function () {
@@ -16,13 +17,28 @@ generatePasswordBtn.addEventListener("click", function () {
 })
 
 function generatePassword() {
-    // randomly get 15 characters from the characters array
+    // randomly get the number of characters specified for the password length
+        // alphabet will always be used
+        // numbers and specialChars will be used depending on if those criteria are checked
+        /* store the characters (to be used to generate the password) in characters array */
     // put them in an array called password
     // after gotten 15 characters, join the characters in password array
     // return the password
 
     const password = []
-    for (let count = 0; count < 15; count++) {
+
+    let characters = alphabet.split("")
+    // console.log(characters)
+    if (includeNums.checked) {
+        characters = characters.concat(numbers.split(""))
+    }
+    if (includeSpecialChars.checked) {
+        characters = characters.concat(specialChars.split(""))
+    }
+
+    console.log(characters)
+
+    for (let count = 0; count < pswdLength.textContent; count++) {
         const randomIndex = Math.floor( Math.random() * characters.length ) + 1
         password.push( characters[randomIndex] )
     }
