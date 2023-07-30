85bd186f2fcfcb6c8b4b6e643e04c26577a861a5
Picasso <urvishp13@gmail.com>
2023-07-29 22:52:42 -0400

    added an alert message to inform user when password copied

--- assets/javascript/copyPassword.js
+++ assets/javascript/copyPassword.js
@@ -6,10 +6,14 @@ console.log(copyIcons)
 copyIcons[0].addEventListener("click", function() {
     /* copy the associated password to the clipboard*/
     navigator.clipboard.writeText(password1.textContent)
+
+    alert("Copied the password")
 })
 
 // when the second copy icon is clicked
 copyIcons[1].addEventListener("click", function() {
     /* copy the associated password to the clipboard*/
     navigator.clipboard.writeText(password2.textContent)
+    
+    alert("Copied the password");
 })
