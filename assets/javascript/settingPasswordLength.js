4122c0497887d8013de83bf3fbb5533ae69b3d5e
Picasso <urvishp13@gmail.com>
2023-09-08 16:16:48 -0400

    removed scroll feature to increase pswd length and made arrowheads active to change pswd length

--- assets/javascript/settingPasswordLength.js
+++ assets/javascript/settingPasswordLength.js
@@ -1,55 +1,15 @@
 const pswdLength = document.getElementById("pswd-length-number")
+const upArrowHead = document.getElementById("up-arrowhead")
+const downArrowHead = document.getElementById("down-arrowhead")
 
-let lastY
-
-/* ---------- FOR MOUSE USERS ---------- */
-
-let clicked = false
-// if the password length field is clicked on
-pswdLength.addEventListener("mousedown", function() {
-    clicked = true
-})
-
-// while the mouse is being dragged after clicking on the password length field 
-document.addEventListener("mousemove", function(e) {
-    if (clicked === true) {
-        alterPswdLength(e.y)
+upArrowHead.addEventListener("click", function() {
+    if (pswdLength.textContent < 15) {
+        pswdLength.textContent++
     }
 })
 
-// once the mouse is released, stop incrementing/decrementing the password length
-document.addEventListener("mouseup", function() {
-    clicked = false
-})
-
-/* ---------- FOR PHONE USERS ---------- */
-
-let touched = false
-// if the password length field is touched
-pswdLength.addEventListener("touchstart", function() {
-    touched = true
-})
-
-// while the finger is being dragged along the screen after clicking on the password length field 
-document.addEventListener("touchmove", function(e) {
-    if (touched === true) {
-        alterPswdLength(e.touches[0].clientY)
+downArrowHead.addEventListener("click", function() {
+    if (pswdLength.textContent > 8) {
+        pswdLength.textContent--
     }
 })
-
-// once the mouse is released, stop incrementing/decrementing the password length
-document.addEventListener("touchend", function() {
-    touched = false
-})
-
-/* ----------------- FUNCTIONS --------------------- */
-
-function alterPswdLength(currentY) {
-    if (currentY < lastY) { // mouse/finger moved up
-        pswdLength.textContent < 15 ? pswdLength.textContent++ : pswdLength.textContent
-    } else { // mouse/finger moved down
-        pswdLength.textContent > 8 ? pswdLength.textContent-- : pswdLength.textContent
-    }
-
-    lastY = currentY
-}
