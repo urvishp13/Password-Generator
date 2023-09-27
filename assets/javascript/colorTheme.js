258d9829b4660b6409c21fd05394aac9b1731af8
Picasso <urvishp13@gmail.com>
2023-09-27 14:41:22 -0400

    wrote code for COLOR THEMES

--- assets/javascript/colorTheme.js
+++ assets/javascript/colorTheme.js
@@ -1,5 +1,12 @@
 const themeSelectBtn = document.getElementById("theme-select")
 const themes = document.getElementById("themes")
+const body = document.body
+
+const light = document.getElementById("light-icon")
+const dark = document.getElementById("dark-icon")
+const system = document.getElementById("system-icon")
+
+const availableThemes = ["light", "dark", "system"]
 
 themeSelectBtn.addEventListener("click", function() {
     themes.classList.toggle("show")
@@ -10,3 +17,14 @@ themes.addEventListener("click", function(e) {
         themes.classList.remove("show")
     }
 })
+
+light.addEventListener("click", () => setTheme("light"))
+dark.addEventListener("click", () => setTheme("dark"))
+system.addEventListener("click", () => setTheme("system"))
+
+function setTheme(theme) {
+    for (const th of availableThemes) {
+        body.classList.remove(th)
+    }
+    body.classList.add(theme)
+}
